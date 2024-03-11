69f4352fd819f4091f91e05b07a5da6c4d89200c
Pedro Sawczuk <pedrosawczuk53@gmail.com>
2024-03-11 10:51:38 -0400

    Relatorio PDF

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from app.views import *
+
+urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
+    path("relatorios/pacientes", PacientesListView.as_view(),
+        name="relat_pacientes"),
+    path('relatorios/pdfpacientes', RelatPdfPacientes.as_view(),
+         name='pdf_pacientes'),
+]
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,37 @@
+from io import BytesIO
 from django.shortcuts import render
+from django.views.generic import *
+from app.models import *
+from io import BytesIO
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.views import View
+from xhtml2pdf import pisa
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'index.html'
+    
+class PacientesListView(ListView):
+    template_name = 'relatorios/pacientes.html'
+    model = Paciente
+    context_object_name = 'pacientes'
+    
+class RelatPdfPacientes(View):
+    
+    def get(self, request):
+        pacientes = Paciente.objects.all()
+        data = {
+            'pacientes': pacientes,
+        }
+        
+        template = get_template("relatorios/pdfpacientes.html")
+        html = template.render(data)
+        result = BytesIO()
+        try:
+            pdf = pisa.pisaDocument(BytesIO(html.encode('UTF-8')), result)
+            return HttpResponse(result.getvalue(),
+                                content_type='application/pdf')
+        except Exception as e:
+            print(e)
+            return None
+            
